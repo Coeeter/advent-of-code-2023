de865a24b040d24d67fb333201e027fe7f662eb3
Nasrullah <nasrullah01n@gmail.com>
2023-12-02 19:38:32 +0800

    Completed day 1

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -1,13 +1,51 @@
 const first = (input: string) => {
-  console.log(input);
-  return 'solution 1';
+  let sum = 0;
+  input.split('\r\n').forEach((line) => {
+    const numbers = line.split('').filter((char) => !isNaN(Number(char)));
+    if (!numbers.length) {
+      return;
+    }
+    sum += Number(numbers.at(0) + numbers.at(-1));
+  });
+  return sum;
 };
 
 const expectedFirstSolution = 'solution 1';
 
 const second = (input: string) => {
-  console.log(input);
-  return 'solution 2';
+  let sum = 0;
+  const lines = input.split('\r\n');
+  const numbersMap = {
+    one: '1',
+    two: '2',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6',
+    seven: '7',
+    eight: '8',
+    nine: '9',
+  };
+  lines.forEach((line) => {
+    const numbers: string[] = [];
+
+    line.split('').forEach((char, i) => {
+      if (!isNaN(Number(char))) {
+        numbers.push(char);
+        return;
+      }
+      const digits = Object.keys(numbersMap).find((key) =>
+        line.slice(i).startsWith(key)
+      );
+      if (!digits) {
+        return;
+      }
+      numbers.push(numbersMap[digits as keyof typeof numbersMap]);
+    });
+
+    sum += Number(numbers.at(0) + numbers.at(-1));
+  });
+  return sum;
 };
 
 const expectedSecondSolution = 'solution 2';
